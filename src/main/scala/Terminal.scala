ee850e63c012f5fd5e1815b09108b7d6bd89cb2e
ymasory <ymasory@gmail.com>
2012-06-01 22:04:46 -0400

    add stubs

--- src/main/scala/Terminal.scala
+++ src/main/scala/Terminal.scala
@@ -1,11 +1,13 @@
 package com.yuvimasory.puritan
 
+import java.io.{ Console => JConsole }
+
 import scalaz.effect.IO
 import scalaz.effect.IO.io
 
 object Terminal {
 
   def readChar: IO[Char] = TODO
-  def readLine: IO[List[Char]] = TODO
-  def readLineString: IO[String] = TODO
+  def readLine: IO[String] = TODO
+  def readLineChars: IO[List[Char]] = TODO
 }
